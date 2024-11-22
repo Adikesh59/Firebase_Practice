cc5bf5334054c690e667f7df7fd306d289cde3ce
Adikesh Master <adikesh59@gmail.com>
2024-11-22 23:47:51 +0530

    Todo 1

--- App.jsx
+++ App.jsx
@@ -1,34 +1,58 @@
 import React,{useEffect, useState} from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput, StyleSheet, Dimensions, StatusBar } from 'react-native';
 import database from '@react-native-firebase/database';
+
 
 export default function App() {
 
-  const [myData,setMyData] = useState("");
-  useEffect(()=>{
-    getDatabase()
-  },[]);
-
-  const getDatabase = async() =>{
-    try{
-      const data = await database().ref('users/1').once('value');
-      console.log(data);
-      setMyData(data.val())
-      
-
-    }catch(err){
-      console.log(err)
+  const [inputTextValue,setInputTextValue] = useState(null);
+  const [list,setList] = useState(null);
+  const handleAddData = async()=>{
+    try {
+      const response = await database().ref("todo/1").set({
+        value: inputTextValue
+      });
+      console.log(response)
+    } catch (error) {
+      console.log(error)
     }
   }
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',backgroundColor:'yellow' }}>
-      
-        <Text>Name: {myData?myData.name:"Loading..."}</Text>
-        <Text>Age: {myData?myData.age:"Loading..."}</Text>
-        <Text>Salary: {myData?myData.post:"Loading..."}</Text> 
-        
-      
+    <View style={styles.container}>
+      <StatusBar hidden={true} />
+      <View>
+        <Text style={{fontSize:30,textAlign:'center',fontWeight:'bold',marginTop:10}}>Todo App</Text>
+      <TextInput style={styles.inputbox}  placeholder='Enter details' 
+      value={inputTextValue}
+      onChangeText={(value)=>setInputTextValue(value)}/>
+      <TouchableOpacity style={styles.btn} onPress={()=>handleAddData()}>
+        <Text style={{fontWeight:'bold',fontFamily:'monospace'}}>Add Data </Text>
+      </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const {height,width} = Dimensions.get('screen');
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:'center'
+  },
+  inputbox:{
+    width:width - 30,
+    borderRadius:15,
+    borderWidth:2,
+    marginTop:30,
+    marginVertical:20,
+    padding:10,
+
+  },
+  btn:{
+    backgroundColor:'#DBD050',
+    alignItems:'center',
+    borderRadius:30,
+    padding:10
+  }
+});
